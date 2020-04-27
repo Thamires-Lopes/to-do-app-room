519e43639e1d5289731a08527ba9c31eca12d20e
Thamires Lopes <thamiresl686@gmail.com>
2020-04-26 21:03:21 -0300

    Initial commit

---
+++ app/src/main/java/com/example/todoapproom/ui/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.todoapproom.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.todoapproom.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
