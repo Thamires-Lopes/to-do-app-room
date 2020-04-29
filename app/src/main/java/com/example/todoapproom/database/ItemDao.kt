41ba41386372ba642139559ec0a2438ea78058b1
Thamires Lopes <thamiresl686@gmail.com>
2020-04-29 13:44:24 -0300

    Adding suspend notation to dao

--- app/src/main/java/com/example/todoapproom/database/ItemDao.kt
+++ app/src/main/java/com/example/todoapproom/database/ItemDao.kt
@@ -7,11 +7,11 @@ import androidx.room.Query
 @Dao
 interface ItemDao {
     @Insert
-    fun addItem(item: Item)
+    suspend fun addItem(item: Item)
 
     @Query("SELECT * FROM Item")
-    fun getAllItems() : List<Item>
+    suspend fun getAllItems() : List<Item>
 
     @Insert
-    fun addMultipleItems(vararg item: Item) //do I need this?
+    suspend fun addMultipleItems(vararg item: Item) //do I need this?
 }
