380b4908f8322356ab2a2399c94301428d0e62ae
Thamires Lopes <thamiresl686@gmail.com>
2020-04-28 21:20:12 -0300

    Creation of basic of detail activity

---
+++ app/src/main/java/com/example/todoapproom/ui/detail/DetailActivity.kt
@@ -0,0 +1,31 @@
+package com.example.todoapproom.ui.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import com.example.todoapproom.R
+import kotlinx.android.synthetic.main.activity_detail.*
+
+class DetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+
+        setSupportActionBar(detail_toolbar)
+        
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item != null) {
+            when (item.itemId) {
+                android.R.id.home -> {
+                    onBackPressed()
+                    return true
+                }
+            }
+        }
+        return false
+    }
+}
